18a38a3dcc91f0baa92b19ecc5f6d6dcc6591110
Sean <102026795+SiunKimm@users.noreply.github.com>
2025-05-31 12:03:37 +0000

    feat: 시간 기능 추가

---
+++ cogs/time.py
@@ -0,0 +1,16 @@
+# time.py
+
+from discord.ext import commands
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+class TimeCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='시간')
+    async def get_time(self, ctx):
+        now = datetime.now(ZoneInfo("Asia/Seoul"))
+        formatted_time = now.strftime("%p %I시 %M분")
+        formatted_time = formatted_time.replace("AM", "오전").replace("PM", "오후")
+        await ctx.send(f"지금은 {formatted_time} 일려나?")
