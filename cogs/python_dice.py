106af233df093c6ce4f30f173e09ab47e97b6c27
Sean <102026795+SiunKimm@users.noreply.github.com>
2025-06-03 12:22:00 +0000

    fix: 기능 텍스트 및 설명 수정

--- cogs/python_dice.py
+++ cogs/python_dice.py
@@ -1,14 +1,14 @@
 # cogs/python_dice.py
+import random
 import discord
 from discord import app_commands
 from discord.ext import commands
-import random
 
 class DiceCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="주사위", description="1부터 6까지의 주사위를 굴립니다.")
+    @app_commands.command(name="주사위", description="운이 있없다")
     async def roll_dice(self, interaction: discord.Interaction):
         num = random.randint(1, 6)
         await interaction.response.send_message(f"🎲 주사위 결과: {num}")
--- cogs/python_time.py
+++ cogs/python_time.py
@@ -9,8 +9,9 @@ class TimeCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="시간", description="현재 시간을 알려드립니다.")
+    @app_commands.command(name="시간", description="현재 시간을 알려드릴까요?")
     async def get_time(self, interaction: discord.Interaction):
         now = datetime.now(ZoneInfo("Asia/Seoul"))
         formatted_time = now.strftime("%p %I시 %M분").replace("AM", "오전").replace("PM", "오후")
         await interaction.response.send_message(f"⏰ 지금은 {formatted_time} 일려나...?")
+        
